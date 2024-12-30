3573b1962891cef9977c71f720dcbbbc18a6c592
Samantha <magnetotail@posteo.net>
2024-12-30 23:06:07 +0100

    Call für disconnect von playern hinzugefügt, damit diese bei einem Tod durch disconnect anschließend entmuted werden

--- player_death_extensions.lua
+++ player_death_extensions.lua
@@ -13,7 +13,7 @@ hook.Add("PostPlayerDeath", "Playerdeath_discord", function(ply, _, __)
                         print("Error during Mute " .. message)
                 end,
                 success = function(body,length,headers,code)
-                    print("POST completed")
+                    print("Sent mute because of death")
                 end,
                 headers = nil,
                 method = "POST",
@@ -35,7 +35,7 @@ hook.Add("TTTEndRound", "unmute_all", function(_)
                     print(message)
                 end,
             success = function(body,length,headers,code)
-                    print("POST completed")
+                    print("Sent unmute all because of round end")
                 end,
             method = "POST",
             url = "http://hartzarett.ruhr:1234/unmute/all",
@@ -57,7 +57,7 @@ hook.Add("TTTBeginRound", "unmute_all_begin", function(_)
                     print(message)
                 end,
             success = function(body,length,headers,code)
-                    print("POST completed")
+                    print("Sent unmute all because of round start")
                 end,
             method = "POST",
             url = "http://hartzarett.ruhr:1234/unmute/all",
@@ -66,3 +66,29 @@ hook.Add("TTTBeginRound", "unmute_all_begin", function(_)
             type = "application/x-www-form-urlencoded"
     })
 end)
+
+gameevent.Listen( "player_disconnect" )
+hook.Add( "player_disconnect", "player_disconnect_unmute", function( data )
+        local name = data.name
+        print(name .. " disconnected, unmuting")
+        if pcall(require, "chttp") and CHTTP ~= nil then
+                my_http = CHTTP
+        else
+                my_http = HTTP
+        end
+
+        my_http({
+                    failed = function(message)
+                            print("Error during Mute " .. message)
+                    end,
+                    success = function(body,length,headers,code)
+                        print("Sent unmute command because of disconnect")
+                    end,
+                    headers = nil,
+                    method = "POST",
+                    url = "http://hartzarett.ruhr:1234/unmute",
+                    type = "application/json",
+                    body = util.TableToJSON({ name = name })
+        })
+
+end )
